81f69a2f2db0b6e352c0350e746651f3b4001fde
venkat-bluespace <veturi@bluespacetech.com>
2018-10-16 13:56:18 -0700

    Changed logging apache logging to slf4j

--- hrms/src/main/java/com/bluespace/tech/hrms/HrmsApplication.java
+++ hrms/src/main/java/com/bluespace/tech/hrms/HrmsApplication.java
@@ -1,7 +1,7 @@
 package com.bluespace.tech.hrms;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,17 +12,17 @@ import com.bluespace.tech.hrms.repositories.client.ClientRepository;
 @SpringBootApplication(scanBasePackages = "com.bluespace.tech.hrms")
 public class HrmsApplication {
 
-	private static Logger logger = LogManager.getLogger(HrmsApplication.class);
+	private static Logger logger = LoggerFactory.getLogger(HrmsApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrmsApplication.class, args);
 		logger.info("Loading HRMS Application");
 	}
-	
+
 	@Bean
 	public CommandLineRunner client(ClientRepository clientRepository) {
 		return null;
-		
+
 	}
 
 }
